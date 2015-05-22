cd7f108f90fea735044c4fb89485b41393e87725
Kyle Mitchell <kyle@kemitchell.com>
2015-05-22 06:44:33 +0000

    Display links for a choice of licenses

--- source/index.js
+++ source/index.js
@@ -1,7 +1,22 @@
+var spdx = require('spdx');
+
+function render(parsed) {
+  if (parsed.license) {
+    return (
+      '<a href="http://spdx.org/licenses/' + parsed.license + '">' +
+      parsed.license +
+      '</a>'
+    );
+  } else {
+    return (
+      render(parsed.left) +
+      ' ' + parsed.conjunction + ' ' +
+      render(parsed.right)
+    );
+  }
+}
+
 module.exports = function(expression) {
-  return (
-    '<a href="http://spdx.org/licenses/' + expression + '">' +
-    expression +
-    '</a>'
-  );
+  var parsed = spdx.parse(expression);
+  return render(parsed);
 };
