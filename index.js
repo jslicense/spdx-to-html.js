1f4f3c3e1c8737843082a4f5a14d77737648ae22
Kyle Mitchell <kyle@kemitchell.com>
2015-08-23 15:52:16 -0700

    Condense code style

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-var spdx = require('spdx');
+var spdx = require('spdx')
 
 function render(parsed) {
   if (parsed.license) {
     if (parsed.license.indexOf('LicenseRef') > -1) {
-      return 'License Reference "' + parsed.license + '"';
-    } else {
+      return 'License Reference "' + parsed.license + '"' }
+    else {
       return (
         '<a href="http://spdx.org/licenses/' + parsed.license + '">' +
           parsed.license +
         '</a>' +
-        (parsed.plus ? ' or newer' : '') +
-        (parsed.exception ? ' with ' + parsed.exception : '')
-      );
-    }
-  } else {
+        ( parsed.plus ? ' or newer' : '' ) +
+        ( parsed.exception ? ' with ' + parsed.exception : '' ) ) } }
+  else {
     return (
       render(parsed.left) +
       ' ' + parsed.conjunction + ' ' +
-      render(parsed.right)
-    );
-  }
-}
+      render(parsed.right) ) } }
 
 module.exports = function(expression) {
   try {
-    var parsed = spdx.parse(expression);
-    return render(parsed);
-  } catch (e) {
-    return null;
-  }
-};
+    var parsed = spdx.parse(expression)
+    return render(parsed) }
+  catch (e) {
+    return null } }
